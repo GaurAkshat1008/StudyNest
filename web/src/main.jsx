c6db3be0ee30290ecb828a0eae2723dc64977ca5
GaurAkshat1008 <akshatgauriiitsurat@gmail.com>
2023-10-12 01:53:15 +0530

    deploying change

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,76 +1,9 @@
 import { ChakraProvider, Flex, Spinner } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import App from "./App.jsx";
+import App from "./App.jsx";
 import theme from "./theme.jsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Navbar from "./component/NavBar.jsx";
-import CreateRoom from "./pages/CreateRoom.jsx";
-import Dashboard from "./pages/Dashboard.jsx";
-import Login from "./pages/Login.jsx";
-import Register from "./pages/Register.jsx";
-import Room from "./pages/room/Room.jsx";
-import { useAuth as authLoader } from "./utils/useAuth.js";
-import Index from "./pages/Index.jsx";
-// import Video from "./pages/call/Video.jsx";
-// import Test from "./pages/test.jsx";
-// import { SocketProvider } from "./context/socket.jsx";
-
-const router = createBrowserRouter([
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/register",
-    element: <Register />,
-  },
-  {
-    loader: authLoader,
-    path: "/dashboard",
-    element: (
-      <>
-        <Navbar /> <Dashboard />{" "}
-      </>
-    ),
-  },
-  {
-    loader: authLoader,
-    path: "/createroom",
-    element: (
-      <>
-        <Navbar /> <CreateRoom />{" "}
-      </>
-    ),
-  },
-  {
-    loader: authLoader,
-    path: "/room/:id",
-    element: (
-      <>
-        <Navbar /> <Room />{" "}
-      </>
-    ),
-  },
-  // {
-  //   loader: authLoader,
-  //   path: "/video",
-  //   element: (
-  //     <>
-  //       <Navbar /> <Video />
-  //     </>
-  //   ),
-  // },
-  {
-    loader: authLoader,
-    path: "/",
-    element: (
-      <>
-        <Navbar /> <Index />
-      </>
-    ),
-  },
-]);
+import { SocketProvider } from "./context/socket.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -82,9 +15,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
           </Flex>
         }
       >
-        {/* <SocketProvider> */}
-        <RouterProvider router={router} />
-        {/* </SocketProvider> */}
+        <SocketProvider>
+          <App />
+        </SocketProvider>
       </React.Suspense>
     </ChakraProvider>
   </React.StrictMode>
